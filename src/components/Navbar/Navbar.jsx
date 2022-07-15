483f53a530d1f12bdb31bcc0e3827cc4a63698de
ikerpaster <ikerpaster@gmail.com>
2022-07-15 22:33:37 +0200

    f

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -13,7 +13,7 @@ const Navbar = () => {
       <div className="app__navbar-logo">
         {/* <img src={images.logo} alt="logo" /> */}
         <span className="logoName">INGA</span>
-        <span className="logonamelast">BIREp</span>
+        <span className="logonamelast">BIREpp</span>
       </div>
       <ul className="app__navbar-links">
         {["home", "about", "work", "skills", "contact"].map((item) => (
