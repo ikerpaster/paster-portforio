e6dab7c9f5200a4ff6f4e21b4d08e75b1167bbd0
ikerpaster <ikerpaster@gmail.com>
2022-07-19 11:27:21 +0200

    updateL

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -11,7 +11,6 @@ const Navbar = () => {
   return (
     <nav className="app__navbar">
       <div className="app__navbar-logo">
-        {/* <img src={images.logo} alt="logo" /> */}
         <span className="logoName">INGA</span>
         <span className="logonamelast">BIRE</span>
       </div>
