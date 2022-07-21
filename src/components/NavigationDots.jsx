2307f0f31ae0d4017b76b4ae2aceaeaed2921ba3
ikerpaster <ikerpaster@gmail.com>
2022-07-21 12:52:58 +0200

    up3

--- src/components/NavigationDots.jsx
+++ src/components/NavigationDots.jsx
@@ -5,14 +5,16 @@ import React from "react";
 
 const NavigationDots = ({ active }) => (
   <div className="app__navigation">
-    {["home", "about", "work", "skills", "contact"].map((item, index) => (
-      <a
-        href={`#${item}`}
-        key={item + index}
-        className="app__navigation-dot"
-        style={active === item ? { backgroundColor: "#313BAC" } : {}}
-      />
-    ))}
+    {["home", "about", "work", "skills", "testimony", "contact"].map(
+      (item, index) => (
+        <a
+          href={`#${item}`}
+          key={item + index}
+          className="app__navigation-dot"
+          style={active === item ? { backgroundColor: "#313BAC" } : {}}
+        />
+      )
+    )}
   </div>
 );
 
